0f200a005f8181e5947aea60913495afd711cdae
edwin <edwin.gongora@hotmail.com>
2019-01-18 16:19:47 -0600

    creación de componentes de following

---
+++ client/src/app/components/following/following.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { Follow } from '../../models/follow';
+import { UserService } from '../../services/user.service';
+import { FollowService } from '../../services/follow.service';
+import { GLOBAL } from '../../services/global';
